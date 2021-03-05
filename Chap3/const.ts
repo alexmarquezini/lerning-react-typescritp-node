4d1c7b518f74bd36b037ef1c84d8db7c925ce9df
Alex Marquezini <mail@alexmarquezini.com>
2021-03-05 09:03:36 -0300

    Learning about ES6 variable types and JavaScript scoping

---
+++ Chap3/const.ts
@@ -0,0 +1,6 @@
+namespace constants {
+    const val1 = 1;
+    val1 = 2; //not work because is constant
+    const val2 = [];
+    val2.push('hello'),
+}
---
+++ Chap3/functionBody.ts
@@ -0,0 +1,8 @@
+if(true){
+    var val1 = 1;
+}
+function go(){
+    var val2 =2;
+}
+console.log(val1);
+console.log(val2); //not work because is out function's scope
